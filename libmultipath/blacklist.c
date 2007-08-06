439b93c260f640f7fcda45dc1f5e06b11ebe6f7a
Hannes Reinecke <hare@suse.de>
2007-08-06 15:29:13 +0200

    Enable cciss support
    Newer cciss controller will be able to support multipathing.
    So we should enable this, too.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/blacklist.c
+++ libmultipath/blacklist.c
@@ -114,12 +114,6 @@ setup_default_blist (struct config * conf)
 		return 1;
 	if (store_ble(conf->blist_devnode, str, ORIGIN_DEFAULT))
 		return 1;
-	
-	str = STRDUP("^cciss!c[0-9]d[0-9]*");
-	if (!str)
-		return 1;
-	if (store_ble(conf->blist_devnode, str, ORIGIN_DEFAULT))
-		return 1;
 
 	vector_foreach_slot (conf->hwtable, hwe, i) {
 		if (hwe->bl_product) {
@@ -136,7 +130,7 @@ setup_default_blist (struct config * conf)
 			}
 		}
 	}
-	
+
 	return 0;
 }
 
