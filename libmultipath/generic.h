1180259d12aa582e12ee18a1950fa39ad41162c6
Martin Wilck <mwilck@suse.com>
2018-02-19 22:42:46 +0100

    generic.h: added rel_ methods, and structured comments

--- libmultipath/generic.h
+++ libmultipath/generic.h
@@ -24,41 +24,87 @@ struct gen_multipath;
 struct gen_pathgroup;
 struct gen_path;
 
+/**
+ * Methods implemented for gen_multipath "objects"
+ */
 struct gen_multipath_ops {
-	/*
-	 * return a newly allocated vector of const struct gen_pathgroup*
-	 * caller is responsible to free it (don't try to free members!)
+	/**
+	 * method: get_pathgroups(gmp)
+	 * caller is responsible to returned data using rel_pathgroups()
+	 * caller is also responsible to lock the gmp (directly or indirectly)
+	 * while working with the return value.
+	 * @param gmp: generic multipath object to act on
+	 * @returns a vector of const struct gen_pathgroup*
 	 */
 	const struct _vector* (*get_pathgroups)(const struct gen_multipath*);
-	/*
-	 * print the property of the multipath map matching
-	 * the passed-in wildcard character into "buf", 0-terminated,
-	 * no more than "len" characters including trailing '\0',
-	 * returning the number of characters printed (without trailing '\0').
-	 * If this wildcard is unsupported, prints nothing and returns 0.
+	/**
+	 * method: rel_pathgroups(gmp, v)
+	 * free data allocated by get_pathgroups(), if any
+	 * @param gmp: generic multipath object to act on
+	 * @param v the value returned by get_pathgroups()
+	 */
+	void (*rel_pathgroups)(const struct gen_multipath*,
+			       const struct _vector*);
+	/**
+	 * method: snprint(gmp, buf, len, wildcard)
+	 * prints the property of the multipath map matching
+	 * the passed-in wildcard character into "buf",
+	 * 0-terminated, no more than "len" characters including trailing '\0'.
+	 *
+	 * @param gmp: generic multipath object to act on
+	 * @param buf: output buffer
+	 * @param buflen: buffer size
+	 * @param wildcard: the multipath wildcard (see print.c)
+	 * @returns the number of characters printed (without trailing '\0').
 	 */
 	int (*snprint)(const struct gen_multipath*,
 		       char *buf, int len, char wildcard);
-	/*
-	 * print special format for multipath printout into buf.
+	/**
+	 * method: style(gmp, buf, len, verbosity)
+	 * returns the format string to be used for the multipath object,
+	 * defined with the wildcards as defined in print.c
+	 * generic_style() should work well in most cases.
+	 * @param gmp: generic multipath object to act on
+	 * @param buf: output buffer
+	 * @param buflen: buffer size
+	 * @param verbosity: verbosity level
+	 * @returns number of format chars printed
 	 */
 	int (*style)(const struct gen_multipath*,
 		     char *buf, int len, int verbosity);
 };
 
+/**
+ * Methods implemented for gen_pathgroup "objects"
+ */
 struct gen_pathgroup_ops {
-	/*
-	 * return a vector of const struct gen_path*
-	 * caller is responsible to free it (don't try to free members!)
+	/**
+	 * method: get_paths(gpg)
+	 * caller is responsible to returned data using rel_paths()
+	 * @param gpg: generic pathgroup object to act on
+	 * @returns a vector of const struct gen_path*
 	 */
 	const struct _vector* (*get_paths)(const struct gen_pathgroup*);
-	/* see above */
+	/**
+	 * method: rel_paths(gpg, v)
+	 * free data allocated by get_paths(), if any
+	 * @param gmp: generic pathgroup object to act on
+	 * @param v the value returned by get_paths()
+	 */
+	void (*rel_paths)(const struct gen_pathgroup*, const struct _vector*);
+	/**
+	 * Method snprint()
+	 * see gen_multipath_ops->snprint() above
+	 */
 	int (*snprint)(const struct gen_pathgroup*,
 		       char *buf, int len, char wildcard);
 };
 
 struct gen_path_ops {
-	/* see above */
+	/**
+	 * Method snprint()
+	 * see gen_multipath_ops->snprint() above
+	 */
 	int (*snprint)(const struct gen_path*,
 		       char *buf, int len, char wildcard);
 };
@@ -75,8 +121,14 @@ struct gen_path {
 	const struct gen_path_ops *ops;
 };
 
-/*
- * Helper functions for setting up xxx_ops
+/**
+ * Helper functions for setting up the various generic_X_ops
+ */
+
+/**
+ * generic_style()
+ * A simple style() method (see above) that should fit most
+ * foreign libraries.
  */
 int generic_style(const struct gen_multipath*,
 		  char *buf, int len, int verbosity);
