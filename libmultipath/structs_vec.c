mwilck/multipath-tools

commit c44d769bbca83b7dfe643712cc783db852a41b87

libmultipath: warn if freeing path that holds mpp->hwe
This just adds an error message to the previous patch.

Signed-off-by: Martin Wilck <mwilck@suse.com>

libmultipath/structs_vec.c
@@ -93,8 +93,11 @@ int adopt_paths(vector pathvec, struct multipath *mpp)
 void orphan_path(struct path *pp, const char *reason)
 {
 	condlog(3, "%s: orphan path, %s", pp->dev, reason);
-	if (pp->mpp && pp->mpp->hwe == pp->hwe)
+	if (pp->mpp && pp->hwe && pp->mpp->hwe == pp->hwe) {
+		condlog(0, "BUG: orphaning path %s that holds hwe of %s",
+			pp->dev, pp->mpp->alias);
 		pp->mpp->hwe = NULL;
+	}
 	pp->mpp = NULL;
 	pp->dmstate = PSTATE_UNDEF;
 	pp->uid_attribute = NULL;

commit 23f956bc1a298730db6cafa6e3d341cec4c5939c

libmultipath: orphan_paths(): avoid BUG message
Since c44d769, we print a BUG message when we orphan a path that
holds the mpp->hwe pointer. But if this called via orphan_paths(),
this is expected and we shouldn't warn.

Fixes: c44d769 ("libmultipath: warn if freeing path that holds mpp->hwe")

libmultipath/structs_vec.c
@@ -124,6 +124,8 @@ void orphan_paths(vector pathvec, struct multipath *mpp, const char *reason)
 	int i;
 	struct path * pp;
 
+	/* Avoid BUG message from orphan_path() */
+	mpp->hwe = NULL;
 	vector_foreach_slot (pathvec, pp, i) {
 		if (pp->mpp == mpp) {
 			if (pp->initialized == INIT_REMOVED) {